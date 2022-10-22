45c437a58ee524039b618bad9d91b57acab87929
sofa <sofiavign@gmail.com>
2022-10-22 08:44:48 -0300

    mobile

--- src/components/card/styled.tsx
+++ src/components/card/styled.tsx
@@ -12,10 +12,18 @@ export const CardStyled = styled.div`
   background-color: white;
   display: flex;
   flex-direction: column;
+
+  @media (max-width: 425px) {
+    width: 200px;
+  }
 `;
 
 export const Title = styled.h1`
   font-size: 40px;
+
+  @media (max-width: 425px) {
+    font-size: 32px;
+  }
 `;
 
 export const Footer = styled.div`
--- src/components/form/styled.tsx
+++ src/components/form/styled.tsx
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const TextField = styled.div`
   display: flex;
   gap: 15px;
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+  }
 `;
